f9048bf6927b459bbb4c0eb73689f5f4eeba3bf9
wilsto <willy.stophe.pro@gmail.com>
2016-02-28 23:27:32 +0100

    0.3.2

--- server/api/tweet/tweet.controller.js
+++ server/api/tweet/tweet.controller.js
@@ -3,15 +3,50 @@
 var _ = require('lodash');
 var Tweet = require('./tweet.model');
 var Twit = require('twit');
+var schedule = require('node-schedule');
+var conf = require('../config.json');
 
+//preparation to tweet
 var T = new Twit({
-    consumer_key: '...',
-    consumer_secret: '...',
-    access_token: '...',
-    access_token_secret: '...',
+    consumer_key: conf.consumer_key,
+    consumer_secret: conf.consumer_secret,
+    access_token: conf.access_token,
+    access_token_secret: conf.access_token_secret,
     timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-})
+});
+var request = require('request').defaults({
+    encoding: null
+});
 
+var j = schedule.scheduleJob('0 9,13,15,17,19,21,23 * * *', function() {
+    console.log('The answer to life, the universe, and everything!');
+});
+
+// retrieve image online
+
+request.get('https://www.mindtools.com/media/Images/Articles/Career_Skills/Key_Career_Points/IS_9810839_slobo_226x150.jpg', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        var b64content = new Buffer(body).toString('base64');
+
+        // first we must post the media to Twitter
+        T.post('media/upload', {
+            media_data: b64content
+        }, function(err, data, response) {
+
+            // now we can reference the media and post a tweet (media will attach to the tweet)
+            var mediaIdStr = data.media_id_string
+            var params = {
+                status: 'loving life #nofilter',
+                media_ids: [mediaIdStr]
+            }
+
+            T.post('statuses/update', params, function(err, data, response) {
+                console.log(data)
+            })
+        })
+
+    }
+});
 
 // Get list of tweets
 exports.index = function(req, res) {
