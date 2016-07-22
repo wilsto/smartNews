7b75cc29893d8fbdac8ca3c0d93310030b45d7eb
wilsto <willy.stophe.pro@gmail.com>
2016-07-22 22:12:46 +0200

    tweet bitly + schedule

--- server/api/tweet/tweet.controller.js
+++ server/api/tweet/tweet.controller.js
@@ -1,14 +1,25 @@
 'use strict';
 
+var auditLog = require('audit-log');
+var config = require('../../config/environment');
+auditLog.addTransport("mongoose", { connectionString: config.mongo.uri });
+//auditLog.addTransport("console");
+
 var _ = require('lodash');
 var Twit = require('twit');
 var schedule = require('node-schedule');
-var TinyUrl = require('nj-tinyurl');
+var BitlyAPI = require("node-bitlyapi");
 
 var conf = require('../config.json');
 
 var Tweet = require('./tweet.model');
 var Article = require('../article/article.model');
+
+var Bitly = new BitlyAPI({
+    client_id: conf.BitlyAPI_client_id,
+    client_secret: conf.BitlyAPI_client_secret
+});
+Bitly.setAccessToken(conf.BitlyAPI_access_token);
 
 //preparation to tweet
 var T = new Twit({
@@ -22,12 +33,6 @@ var T = new Twit({
 var request = require('request').defaults({
     encoding: null
 });
-
-var j = schedule.scheduleJob('0 9,13,15,17,19,21,23 * * *', function() {
-    console.log('The answer to life, the universe, and everything!');
-});
-
-
 
 var articleToShare = function() {
     var query = {
@@ -47,9 +52,9 @@ var articleToShare = function() {
                     return o.score;
                 }))
             });
-            var shortenUrl = TinyUrl.shorten(articles[0].link, function(err, url) {
+            Bitly.shortenLink(articles[0].link, function(err, results) {
                 //Returns a shorter version of http://google.com - http://tinyurl.com/2tx
-
+                var BitlyURL = JSON.parse(results);
                 request.get(articles[0].image || articles[0]._feed.image, function(error, response, body) {
                     if (!error && response.statusCode === 200) {
                         var b64content = new Buffer(body).toString('base64');
@@ -62,20 +67,28 @@ var articleToShare = function() {
                             // now we can reference the media and post a tweet (media will attach to the tweet)
                             var mediaIdStr = data.media_id_string;
                             var TwitText = articles[0].title;
-                            console.log('articles[0].title', articles[0].title.length);
-                            TwitText += ' @' + articles[0]._feed.twitter;
+                            //TwitText += ' @' + articles[0]._feed.twitter;
                             TwitText += (articles[0].subarea.toLowerCase() !== biggestTerm[0].term.toLowerCase()) ? ' #' + articles[0].subarea + ' #' + biggestTerm[0].term : ' #' + articles[0].subarea;
-                            TwitText += ' - ' + url;
-                            console.log('TwitText', TwitText);
-                            console.log('TwitTextLength', TwitText.length);
+                            TwitText += ' - ' + BitlyURL.data.url;
                             var params = {
                                 status: TwitText,
                                 media_ids: [mediaIdStr]
                             }
 
                             T.post('statuses/update', params, function(err, data, response) {
-                                (err) ? console.log('err', err): console.log('tweet envoyé');
+                                if (err) {
+                                    auditLog.logEvent('Error', 'tweet.controller.js - function articleToShare', 'T.post/statuses/update', TwitText, 'err', err);
+                                } else {
+                                    console.log('tweet envoyé');
+                                    var updatedArticle = articles[0];
+                                    updatedArticle.shared = true;
+                                    Article.findOneAndUpdate({ _id: updatedArticle._id }, updatedArticle, { new: true }).exec().then(function(updarticle) {
+                                        auditLog.logEvent('Event', 'tweet.controller.js - function articleToShare', 'T.post/statuses/update', TwitText, 'Article mis à jour', 'Article mis à jour et tweeté');
+                                    });
+                                }
+
                             })
+
                         })
 
                     }
@@ -87,7 +100,13 @@ var articleToShare = function() {
     });
 
 };
-articleToShare();
+//articleToShare();
+var j = schedule.scheduleJob('0 9,13,15,17,19,21,23 * * *', function() {
+    setTimeout(function() {
+        console.log('The answer to life, the universe, and everything!');
+        articleToShare();
+    }, Math.floor(Math.random() * 1000 * 60 * 45));
+});
 
 // Get list of tweets
 exports.index = function(req, res) {
